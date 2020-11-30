86e669d36e151590e72800295e59ba1fb7328928
Keti <ketevan.mdzinarishvili.1@btu.edu.ge>
2020-11-30 23:37:13 +0400

    web.php changed

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::get('/users/login', [\App\Http\Controllers\LoginController::class, 'login
 Route::post('/users/post-login', [\App\Http\Controllers\LoginController::class, 'postLogin'])->name("post.login");
 
 
-//Route::middleware('auth')->group(function(){
+Route::middleware('auth')->group(function(){
 //list all routes here
     Route::get('/',[\App\Http\Controllers\QuizController::class, 'quiz'])->name('quiz');
     Route::get('/create',[\App\Http\Controllers\QuizController::class, 'create'])->name('create');
     Route::put('/post-create',[\App\Http\Controllers\QuizController::class, 'postCreate'])->name('post.create');
     Route::post('/results', [\App\Http\Controllers\QuizController::class, 'postQuiz'])->name('post.quiz');
-    //});
+    });
 
 
